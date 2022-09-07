2bdd93d30bdd034105ba40ba8c798b6956a599ea
CebollaRodriguez <sangabo777@gmail.com>
2022-09-06 22:24:10 -0500

    implement dependencies complete

--- app/src/main/java/com/example/pokedex/PokedexApp.kt
+++ app/src/main/java/com/example/pokedex/PokedexApp.kt
@@ -1,4 +1,7 @@
 package com.example.pokedex
 
-class PokedexApp {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PokedexApp : Application()
---
+++ app/src/main/java/com/example/pokedex/core/NetworkModule.kt
@@ -0,0 +1,25 @@
+package com.example.pokedex.core
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://pokeapi.co/api/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+
+}
