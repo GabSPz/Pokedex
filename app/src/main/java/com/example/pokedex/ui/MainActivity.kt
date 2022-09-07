e9aecb754fa9c52c85cb01cdf435a374365fbaf6
CebollaRodriguez <sangabo777@gmail.com>
2022-09-06 22:17:07 -0500

    no message

--- app/src/main/java/com/example/pokedex/ui/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.pokedex
+package com.example.pokedex.ui
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.pokedex.R
 import com.example.pokedex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
